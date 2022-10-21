216f8c0e91a7cd7e74e5f00fcc2b24c79a951e29
Natalie <hairnatalie7@gmail.com>
2022-10-21 15:08:54 -0600

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+    <header>
+    <h2>Natalie's Movie App</h2>
+    </header>
+    )
+}
+
+
+export default Header
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,28 @@
+import React from "react"; 
+   import './MovieCard.css'; 
+
+const MovieCard = ({movie, addMovie, removeMovie, list}) => {
+    const inWatchlist = list.filter ((mov) =>{
+        return mov.id===movie.id
+    });
+
+   
+    const button = 
+    inWatchlist.length===0 
+    ? (<button onClick ={()=> addMovie(movie)}>Add to List</button>
+    ): (
+        <button onClick ={() => removeMovie(movie)}>Remove</button>
+    );
+    return (
+        <div className="movie-card">
+            <div>
+                <img style={{width: 200, height: 300}} src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
+                <h3>{movie.original_title}</h3>
+            </div>
+            {button}
+        </div>
+    )
+};
+
+
+export default MovieCard; 
